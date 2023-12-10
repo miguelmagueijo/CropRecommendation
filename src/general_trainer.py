024be7f38a6210b26cc3eb39fcb9a4c72756cb69
Miguel Magueijo <miguelmagueijo@hotmail.com>
2023-12-10 00:07:55 +0000

    Update general_trainer.py variable names, add author and desc

--- src/general_trainer.py
+++ src/general_trainer.py
@@ -1,15 +1,25 @@
+"""
+Author: Miguel Magueijo
+Description:
+    This file trains a Random Forest classifier for each specified dataset and shows it's evaluation metrics.
+
+"""
+
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 
 import pandas as pd
 
-path_prefix = "../Data/"
-filenames = ["AtharvaIngle_CR.csv", "Kaggle_Cleaned.csv", "RaulSingh_CR.csv", "Harvard_Cleaned.csv",
-             "KaranNisar_CR.csv", "Wandb_Cleaned.csv"]
+PATH_PREFIX = "../Data/"
+FILES_EXTENSION = ".csv"
+FILENAMES = ["AtharvaIngle_CR", "Kaggle_Cleaned", "RaulSingh_CR", "Harvard_Cleaned",
+             "KaranNisar_CR", "Wandb_Cleaned", "Combined_CR", "Kaggle_Harvard_Combined",
+             "KaggleLabels_Harvard_Combined", "Kaggle_HarvardLabels_Combined"]
 
-for f in filenames:
-    csv_data = pd.read_csv(path_prefix + f)
+
+for f in FILENAMES:
+    csv_data = pd.read_csv(PATH_PREFIX + f + FILES_EXTENSION)
 
     print(f"\n\n\nCurrent file: {f}")
 
